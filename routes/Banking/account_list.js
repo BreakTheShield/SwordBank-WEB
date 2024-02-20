98ef936f2dcfeaab828752f363fcebaa9e6c8d68
twotrillion <149655719+twotrillion@users.noreply.github.com>
2024-02-20 18:22:28 +0900

    make new account

--- routes/Banking/account_list.js
+++ routes/Banking/account_list.js
@@ -32,4 +32,32 @@ router.get('/', checkCookie, function (req, res) {
     });
 });
 
+
+router.post('/create', checkCookie, function (req, res) {
+    const cookie = req.cookies.Token;
+    
+    console.log(cookie);
+    profile(cookie).then(profileData => {
+        console.log("account_list_post에서의 profileData : ",profileData);
+        axios({
+            method: "post",
+            url: api_url + "/api/account/create",
+            headers: {"authorization": "1 " + cookie}
+        }).then((data) => {
+
+            let result = decryptRequest(data.data).data;
+            console.log("accout new create result : ",result);
+
+            return res.render("Banking/account_list", {html_data: result, pending: profileData, select: "account_list"});
+        }).catch(function (error) {
+
+            var html_data = [
+                 { balance: error, account_number: error, bank_code: error }
+            ];
+
+            return res.render("Banking/account_list", {html_data: html_data, pending: profileData, select: "account_list"});
+        });
+    });
+});
+
 module.exports = router;
