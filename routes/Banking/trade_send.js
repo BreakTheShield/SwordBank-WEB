17e37c92192ae9c0985397092d5c266aedf78d87
jung-seung-hwan <94040225+jung-seung-hwan@users.noreply.github.com>
2024-02-21 13:17:15 +0900

    송금하기에 보낼계좌 선택

--- routes/Banking/trade_send.js
+++ routes/Banking/trade_send.js
@@ -17,7 +17,16 @@ router.get("/", checkCookie, async (req, res) => {
         }).then((data2) => {
             var d = decryptRequest((data2.data));
             var results = d.data.accountdata;
-            var html_data = `<input type="text" class="form-control form-control-user" autocomplete="off" id="drop" name="to_account" placeholder="대상 계좌번호" list="dropdown"> <datalist id="dropdown">`
+            var html_data = `
+                <input type="text" class="form-control form-control-user" autocomplete="off" id="drop_from" name="from_account" placeholder="보내는 계좌번호" list="dropdown_from">
+                <datalist id="dropdown_from">`;
+            results.forEach(function (a) {
+                html_data += `<option value="${a}"></option>`;
+            });
+
+            html_data += `</datalist>`;
+
+            html_data += `<input type="text" class="form-control form-control-user" autocomplete="off" id="drop" name="to_account" placeholder="대상 계좌번호" list="dropdown"> <datalist id="dropdown">`
 
             results.forEach(function (a) {
                 html_data += `<option value= ${a}></option>`;
@@ -33,6 +42,7 @@ router.post("/post", checkCookie, function (req, res, next) {
     let json_data = {};
     let result = {};
 
+    json_data['from_account'] = parseInt(req.body.from_account);
     json_data['to_account'] = parseInt(req.body.to_account);   //데이터가 숫자로 들어가야 동작함
     json_data['amount'] = parseInt(req.body.amount);
     json_data['sendtime'] = seoultime;
