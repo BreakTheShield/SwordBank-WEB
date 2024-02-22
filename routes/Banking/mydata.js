28d4c3db56ba15c292c9ba5083aa772ccec80d7a
RyuSeung <48815923+RyuSeung@users.noreply.github.com>
2024-02-22 11:11:24 +0900

    mydata test2

--- routes/Banking/mydata.js
+++ routes/Banking/mydata.js
@@ -18,6 +18,34 @@ router.get('/', checkCookie, function (req, res) {
     });
 });
 
+// router.post('/', checkCookie, function (req, res) {
+//     const cookie = req.cookies.Token;
+    
+//     console.log(cookie);
+//     profile(cookie).then(profileData => {
+//         console.log("account_list_post에서의 profileData : ",profileData);
+//         axios({
+//             method: "post",
+//             url: api_url + "/api/mydata/view",
+//             headers: {"authorization": "1 " + cookie}
+//         }).then((data) => {
+
+//             let result = decryptRequest(data.data).data;
+//             console.log("accout new create result : ",result);
+
+//             return res.render("Banking/mydata", {html_data: result, pending: profileData, select: "mydata"});
+//         }).catch(function (error) {
+
+//             var html_data = [
+//                  { username: error, balance: error, account_number: error, bank_code: error }
+//             ];
+
+//             return res.render("Banking/mydata", {html_data: html_data, pending: profileData, select: "mydata"});
+//         });
+//     });
+// });
+
+// 외부 api test
 router.post('/', checkCookie, function (req, res) {
     const cookie = req.cookies.Token;
     
@@ -26,7 +54,7 @@ router.post('/', checkCookie, function (req, res) {
         console.log("account_list_post에서의 profileData : ",profileData);
         axios({
             method: "post",
-            url: api_url + "/api/mydata/view",
+            url: api_url + "/api/mydata/getdata",
             headers: {"authorization": "1 " + cookie}
         }).then((data) => {
 
@@ -44,6 +72,5 @@ router.post('/', checkCookie, function (req, res) {
         });
     });
 });
-
 
 module.exports = router;
