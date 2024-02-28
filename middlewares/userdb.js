bfff2c31c5e6b7a394f01ae12fc49c6d15e81d9e
dudals9095 <59114335+dudals9095@users.noreply.github.com>
2024-02-28 15:30:23 +0900

    Update userdb.js

--- middlewares/userdb.js
+++ middlewares/userdb.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql2");
 var userdb = mysql.createConnection({
-  host: "rds-shield.czmsy2g2m4cg.ap-northeast-2.rds.amazonaws.com",
+  host: "rds-sword.czmsy2g2m4cg.ap-northeast-2.rds.amazonaws.com",
   user: "root",
   password: "gozld13!#",
   database: "dvba",
