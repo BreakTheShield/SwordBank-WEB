6939b236cb2c153341416e2a6682b28aeaf64ee3
dudals9095 <59114335+dudals9095@users.noreply.github.com>
2024-02-28 15:30:09 +0900

    Update db.js

--- middlewares/db.js
+++ middlewares/db.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql2");
 var db = mysql.createConnection({
-  host: "",
+  host: "rds-sword.czmsy2g2m4cg.ap-northeast-2.rds.amazonaws.com",
   user: "root",
   password: "gozld13!#",
   database: "board",
